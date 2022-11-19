451d8a38cd53760b5a23abaee6309df2339999fb
kumar ankit <kumar.ankit@nuance.com>
2022-11-19 17:38:24 +0530

    added authentication

---
+++ src/main/java/com/nuance/quiz/configuration/UserDetailsServiceImpl.java
@@ -0,0 +1,30 @@
+package com.nuance.quiz.configuration;
+
+
+import com.nuance.quiz.repository.UserRepository;
+import java.util.Collections;
+import org.springframework.context.annotation.Primary;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@Primary
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    public UserDetailsServiceImpl(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        com.nuance.quiz.entity.User user = userRepository.findByUserEmail(username)
+            .orElseThrow(() -> new UsernameNotFoundException("User not found"));
+        return new User(user.getUserEmail(), user.getPassword(), Collections.emptyList());
+    }
+}
